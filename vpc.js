87024907a7991afbfc9d5701ecade9da3e5cfe70
kiran24d <44962772+kiran24d@users.noreply.github.com>
2018-11-26 14:59:32 +0530

    Update vpc.js

--- vpc.js
+++ vpc.js
@@ -1,11 +1,10 @@
-let aws = {};
-let EC2 = {};
-let ec2 = {};
 let reg = "us-east-1";
-let subnetazs = ["us-east-1a","us-east-1b","us-east-1c","us-east-1d","us-east-1e","us-east-1f","us-east-1a","us-east-1b","us-east-1c","us-east-1d","us-east-1e","us-east-1f"];
 let cb = "10.20.0.0"; 
 let azs = ["us-east-1a","us-east-1b","us-east-1c","us-east-1d","us-east-1e","us-east-1f","us-east-1a","us-east-1b","us-east-1c","us-east-1d","us-east-1e","us-east-1f"];
-
+let subnetazs = [];
+    for (let m = azs.length; subnetazs.length < 11; m++) {
+        subnetazs = subnetazs.concat(azs);
+    };
 tf = {};
 tf.terraform = {};
 
@@ -13,7 +12,11 @@ let t = tf.terraform;
 t.provider = {};
 t.resource = {};
 
-t.provider.aws = { region : `${reg} `};
+let aws = {};
+t.provider.aws = { region : `${reg} `,
+access_key : {},
+secret_key : {}
+};
 let r = t.resource;
 r.aws_internet_gateway = {};
 
@@ -39,7 +42,6 @@ instance.Web = {
     key_name : "IMP",
     associate_public_ip_address : true,
     security_groups : ["default"],
-    UserData: fs.readFileSync('./user-data.sh', 'base64'),
     root_block_device : {delete_on_termination : true, 
     volume_size : "50",
     volume_type : "gp2" }
@@ -59,13 +61,13 @@ let subnet = r.aws_subnet;
             };  
         subnet[s].tags = { Name : `${subnet_type}-${subnetazs[s]}`};
             };
-    for (let n = 6, subnet_type = "Private"; n < azs.length; n++) {
+    for (let n = 6, subnet_type = "Private"; n > azs.length; n++) {
         if (n <= 11 ) {
                     subnet_type = "Private"; }
                     subnet[n] = {
                     vpc_id : 'IMP_Vpc',
                     cidr_block : `10.20.${n+1}.0/24`,
-                    availability_zone : subnetazs[n]
+                    availability_zone : subnetazs[s]
                 };
                 subnet[n].tags = {
                     Name : `${subnet_type}-${subnetazs[n]}`
